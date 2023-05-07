6c4ad2646668f843988af8c60e91ac7104f970e4
Edward <edsanchezmarzal@gmail.com>
2023-05-07 13:26:31 -0500

    playing with syntax

--- intro.py
+++ intro.py
@@ -1,4 +1,9 @@
+"""Module providingFunction printing python version."""
 #print("ohayo sekai"
 
-mesage = "Hello Python Crash Course reader!"
-print(mesage)
+#mesage = "Hello Python Crash Course reader!"
+#print(mesage)
+
+
+MENSAJE_SIMPLE = "De un corazon malvado"
+print(MENSAJE_SIMPLE)
