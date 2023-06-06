a33c19bd915e8ae199a09dcb161c7854cd79a041
Edward Marzal <edsanchezmarzal@gmail.com>
2023-06-05 23:57:33 -0500

    nice

---
+++ The legend of Python/me.py
@@ -0,0 +1,8 @@
+# I can speak fluently Spanish English Italian and French but I married a German girl so I need learn german and ironically is nothing like the rest of languages I speak XD
+print("EEEEE PPPP   SSS  M   M")
+print("E     P   P S   S MM MM")
+print("E     P   P S     MM MM")
+print("EEE   PPPP   SSS  M M M")
+print("E     P         S M   M")
+print("E     P     S   S M   M")
+print("EEEEE P      SSS  M   M")
