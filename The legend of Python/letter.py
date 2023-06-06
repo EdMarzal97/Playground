747209322c63147b9190fe72d5373c8a817f0f5e
Edward Marzal <edsanchezmarzal@gmail.com>
2023-06-06 00:09:07 -0500

    a letter for me

---
+++ The legend of Python/letter.py
@@ -0,0 +1,9 @@
+# Today's date.
+# How you are feeling at this moment.
+# What you want to accomplish during this course.
+# A little message to your older, wiser, and programmer self.
+# Your favorite emoji to spice things up!
+
+print(
+    " Today 06/06/2023 , Being without sleep since yesterday I can say I feel amazing, I feel motivated and with discipline ill reach my goal of become a senior software developer "
+)
